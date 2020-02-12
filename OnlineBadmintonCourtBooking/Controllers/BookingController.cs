65260d6361681af2ec33e5be16b0048cbca83f77
Karthika D <karthikacseksrct@gmail.com>
2020-02-12 18:29:19 +0530

    Add project files.

---
+++ OnlineBadmintonCourtBooking/Controllers/BookingController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineBadmintonCourtBooking.Controllers
+{
+    public class BookingController : Controller
+    {
+        // GET: Booking
+        public string Index()
+        {
+            return "Welcome user";
+        }
+    }
+}
---
+++ OnlineBadmintonCourtBooking/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="OnlineBadmintonCourtBooking.MvcApplication" Language="C#" %>
